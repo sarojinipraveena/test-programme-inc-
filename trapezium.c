9ab1e79b0adc1b09ed2ce78812edd0c300caee33
sarojinipraveena <sarojinipraveena@gmail.com>
2023-11-07 13:40:17 +0530

    forth commit

---
+++ trapezium.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,h;
+    float area;
+    printf("enter the area for the area and base and height for the trapezium\n:");
+    scanf("%f%f%f",&a,&b, &h);
+    area = 0.5 * (a + b) * h ;
+    printf("Area of the trapezium is: %.3f", area);
+    return 0;
+}
+
