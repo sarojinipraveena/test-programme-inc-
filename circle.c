843cb491f88fbb4aa4ab51ac3637fddc0edeca54
sarojinipraveena <sarojinipraveena@gmail.com>
2023-11-07 12:01:48 +0530

    third commit

---
+++ circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float pie=3.14;
+    int radius
+    printf("enter the radius of circle\n:");
+    scanf("%d",&radius);
+    printf("the radius of the circle is %d\n",raduius);
+    area=(float)(pie*radius*radius);
+    printf("area of givemn circle is %f",area);
+    return 0;
+}
