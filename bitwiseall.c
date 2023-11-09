1c98082412681e6a20fdf602ef8c029b8ad0644e
sarojinipraveena <sarojinipraveena@gmail.com>
2023-11-09 12:58:10 +0530

    seventh commit

---
+++ bitwiseall.c
@@ -0,0 +1,18 @@
+//USING ALL BITWISE OPERATORS
+#include <stdio.h>
+int main()
+{
+    //DECLARING VARIABLES
+    int a,b;
+    printf("Enter the value\n");
+    scanf("%d",&a);
+    printf("Enter the value\n");
+    scanf("%d",&b);
+    printf("a&b =%d\n",a&b);
+    printf("a|b =%d\n",a|b);
+    printf("a^b =%d\n",a^b);
+    printf("~a =%d\n",~a);
+    printf("a<<b =%d\n",a<<b);
+    return 0;
+
+}
